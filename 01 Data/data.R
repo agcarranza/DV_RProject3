f7700732bba52aa4d87dbf0a83bbac8afa7ef224
Gael <carranzagael@gmail.com>
2015-03-01 17:23:29 -0600

    first commit

---
+++ 01 Data/data.R
@@ -0,0 +1,9 @@
+require(ggplot2)
+require(plyr)
+require(grid)
+require(RCurl)
+require(reshape2)
+require(tidyr)
+require(dplyr)
+
+df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from LIFE_EXPECTANCY"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_agc846', PASS='orcl_agc846', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
